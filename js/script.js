297b1940cb10747d7f996df892a4e0b38d5e035a
rubigit <rcortes1708.rcj@gmail.com>
2021-06-10 13:18:19 -0400

    Add comments to the code

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
 	console.log(`Ready`)
 
+	// Array of pictures to display depeding on the Love Calculator Result"
 	const pictures = [
-		{ picName: `Broken_heart.png`, alt: `Brokenheart` },
+		{ picName: `Broken_heart.png`, alt: `Brokengit s` },
 		{ picName: `emptyheart.png`, alt: `empty heart` },
 		{ picName: `happyheart.png`, alt: `happy heart` },
 		{ picName: `shappyheart.png`, alt: `super happy heart` }
 	]
 
+	//Select and storage HTML elements
 	const loveForm = document.querySelector(`#loveForm`)
 	const fname = document.querySelector(`#fname`)
 	const sname = document.querySelector(`#sname`)
 	const newCalc = document.querySelector(`#newLoveCalc`)
 	const loveResult = document.querySelector(`#resultPanel`)
 
+	//Function to clear input's values
 	const clearForm = () => {
 		fname.value = ""
 		sname.value = ""
 	}
 
+	// Function to hide or display Preloading picture
 	const toggleLoader = function () {
 		const lder = document.querySelector(`#loader`)
 		lder.classList.toggle(`hide`)
 		lder.classList.toggle(`loader`)
 	}
+	// Function to hide or display Result panel
 	const toggleResult = function () {
 		loveResult.classList.toggle(`hide`)
 		loveResult.classList.toggle(`resultPanel`)
 	}
+
+	// Function to hide or display the Form
 	const toggleForms = function () {
 		loveForm.classList.toggle(`hide`)
 		loveForm.classList.toggle(`loveForm`)
@@ -36,11 +43,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	}
 
+	//Function to create elements to display
+	//the result of the API (calculateLove())
 	function printLoveResult(fname, sname, per, res) {
 		toggleLoader()
 		let srcpic = ""
 		let altpic = ""
-
+		//Set the name and description of the picture to display 
+		//based on the percetaje rate result
 		if (per <= 30) {
 			srcpic = pictures[0].picName
 			altpic = pictures[0].alt
@@ -55,13 +65,14 @@ document.addEventListener("DOMContentLoaded", function () {
 			altpic = pictures[3].alt
 		}
 
+		//Populate the Love Result Panel
 		loveResult.innerHTML = `
-		<p class="pgraphResult"><span>Percentage of love between:</span><span><b>${fname.toUpperCase()}</b> and <b>${sname.toUpperCase()}</b></span><span class="percent">${per}%</span></p>
+		<p class="pgraphResult"><span>Percentage of love between:</span><span><b>${fname.toUpperCase()}</b> and <b>${sname.toUpperCase()}</b></span><span class="percent">${per}%</span><span>"${res}"</span></p>
 		<div class="heartContainer">
-		<p>${res}</p>
 		<img class="heart" src="./img/${srcpic}" alt="${altpic}">
 		</div>
 		`
+		//Create a button and add it to Love Result Panel to Make a new calculation
 		const newLoveCalc = document.createElement(`button`)
 		newLoveCalc.classList.add(`newLoveCalc`)
 		newLoveCalc.setAttribute(`id`, `newLoveCalc`)
@@ -70,8 +81,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		loveResult.appendChild(newLoveCalc)
 	}
 
+	// Function to call the API: Love Calculator
 	function calculateLove() {
-
 		fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname.value}&sname=${sname.value}`, {
 			"method": "GET",
 			"headers": {
@@ -81,6 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		})
 			.then((response) => response.json())
 			.then(response => {
+				//call function to print the results
 				printLoveResult(response.fname, response.sname, Number(response.percentage), response.result)
 			})
 			.catch(err => {
@@ -88,6 +100,8 @@ document.addEventListener("DOMContentLoaded", function () {
 			});
 	}
 
+	//function that call the functions that hide Preloader and Forms.
+	// Display the Result panel with delay
 	function runPreLoader() {
 		let timing = ``
 		toggleLoader()
@@ -95,6 +109,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		timing = setTimeout(function () { toggleResult() }, 1000);
 	}
 
+	//Function to alert when one of the input's values are empty	
 	function makeAlert() {
 		fname.classList.add(`alert`)
 		sname.classList.add(`alert`)
@@ -103,16 +118,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	}
 
+	//Function to make the submit button clicable
 	loveForm.addEventListener("submit", function (event) {
 		event.preventDefault()
 		if (fname.value && sname.value) {
+			//Call funtion that display the Preloader
 			runPreLoader()
+			//Call function that calls the API
 			calculateLove()
-			// toggleForms()
 		} else {
 			makeAlert()
 		}
-
 	})
-
 })
